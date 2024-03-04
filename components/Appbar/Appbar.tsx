a4e35038566f79ba89ed736dccdb199197c1be51
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-03-04 02:42:56 +0000

    Update Appbar component with min-height

--- components/Appbar/Appbar.tsx
+++ components/Appbar/Appbar.tsx
@@ -4,7 +4,7 @@ import SignInBtn from "@/components/signin-btn/signin-btn";
 
 const Appbar = () => {
   return (
-    <header className='flex gap-4 p-4 bg-gradient-to-b from-card to-card/90 shadow text-white fixed w-full'>
+    <header className='flex gap-4 p-4 bg-gradient-to-b from-card to-card/90 shadow text-white fixed w-full min-h-20'>
       <SignInBtn />
     </header>
   );
